53dd087bfd05667c2b82cf44d12eb1ffc1c779fc
rendy278 <kepolaumautauaja@gmail.com>
2024-05-31 20:24:40 +0700

    add animate load

--- src/app/diagnosis/page.jsx
+++ src/app/diagnosis/page.jsx
@@ -1,6 +1,8 @@
 "use client";
 import React, { useState } from "react";
 import { questions } from "@/constants/questions";
+import { treatment } from "@/constants/treatment";
+import { Solusi } from "@/constants/article";
 import Title from "@/components/Title";
 
 const Page = () => {
@@ -10,6 +12,7 @@ const Page = () => {
   const [error, setError] = useState("");
   const [confirmationVisible, setConfirmationVisible] = useState(false);
   const [resultVisible, setResultVisible] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const handleAnswerChange = (key, value) => {
     setAnswers((prevAnswers) => ({ ...prevAnswers, [key]: value }));
@@ -40,24 +43,60 @@ const Page = () => {
     }, 0);
 
     return sum > 10 ? (
-      <p className="font-bold text-base dark:text-slate-100">
-        Anda mengalami gejala yang mengindikasikan kemungkinan penyakit kanker.
-        Segera konsultasikan dengan dokter untuk diagnosis dan perawatan lebih
-        lanjut.
-      </p>
+      <div className="h-fit overflow-hidden  md:h-96 md:overflow-auto lg:h-60 lg:overflow-auto ">
+        <p className="font-bold text-base dark:text-slate-100">
+          Anda mengalami gejala yang mengindikasikan kemungkinan penyakit
+          kanker. Segera konsultasikan dengan dokter untuk diagnosis dan
+          perawatan lebih lanjut.
+        </p>
+        <div className="mt-4">
+          <h2 className="text-lg font-bold">Rekomendasi Perawatan:</h2>
+          <div className="text-start ml-5 mt-2 ">
+            {treatment.map((item, index) => (
+              <ul key={index}>
+                <span className="flex gap-3">
+                  {item.id} {item.title}
+                </span>
+                <li>{item.description}</li>
+              </ul>
+            ))}
+          </div>
+        </div>
+      </div>
     ) : (
-      <p className="font-bold text-green-500 dark:text-green-400">
-        Anda tidak mengalami gejala yang mengindikasikan penyakit kanker.
-        Tetaplah memperhatikan kesehatan Anda secara umum.
-      </p>
+      <div className="h-fit overflow-hidden md:h-96 md:overflow-auto lg:h-60 lg:overflow-auto ">
+        <p className="font-bold text-green-500 dark:text-green-400">
+          Anda tidak mengalami gejala yang mengindikasikan penyakit kanker.
+          Tetaplah memperhatikan kesehatan Anda secara umum.
+        </p>
+        <div className="mt-4">
+          <h2 className="text-lg font-bold">
+            Solusi Pencegahan Penyakit Kanker:
+          </h2>
+          <div className="text-start ml-5 mt-2 ">
+            {Solusi.map((item, index) => (
+              <ul key={index}>
+                <span className="flex gap-3">
+                  {item.id} {item.title}
+                </span>
+                <li>{item.solusi}</li>
+              </ul>
+            ))}
+          </div>
+        </div>
+      </div>
     );
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const result = calculateResult();
-    setDiagnosis(result);
-    setResultVisible(true);
+    setLoading(true);
+    setTimeout(() => {
+      const result = calculateResult();
+      setDiagnosis(result);
+      setResultVisible(true);
+      setLoading(false);
+    }, 3000);
   };
 
   const handleReset = () => {
@@ -69,8 +108,8 @@ const Page = () => {
   };
 
   return (
-    <section className="mx-auto px-5 py-5">
-      <div className="flex flex-col h-screen dark:text-slate-100 justify-center gap-4 items-center">
+    <section className="mx-auto h-fit lg:h-screen md:h-screen flex items-center justify-center px-5 ">
+      <div className="flex flex-col h-fit my-40 lg:my-20 dark:text-slate-100 justify-center gap-4 items-center">
         <Title title="Diagnosis Penyakit Kanker" />
         <p className="text-red-500 dark:text-red-400 font-bold text-center text-xl">
           Ayo Cari Tahu Apakah Ada Penyakit Kanker Pada Tubuh Anda atau tidak
@@ -136,48 +175,75 @@ const Page = () => {
               </div>
             </>
           ) : (
-            <div className="mx-auto items-center">
+            <div className="mx-auto h-full items-center">
               <div className="mt-8">
-                {resultVisible && (
-                  <p className="font-bold text-red-500 dark:text-red-400 flex flex-col text-lg text-center gap-3">
-                    Hasil Diagnosis: <br />
-                    <span className="text-gray-800 dark:text-gray-100">
-                      {diagnosis}
-                    </span>
-                  </p>
+                {loading ? (
+                  <div className="flex justify-center">
+                    <svg
+                      className="animate-spin h-20 w-20 text-red-500"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                    >
+                      <circle
+                        className="opacity-25"
+                        cx="12"
+                        cy="12"
+                        r="10"
+                        stroke="currentColor"
+                        strokeWidth="4"
+                      ></circle>
+                      <path
+                        className="opacity-75"
+                        fill="currentColor"
+                        d="M4 12a8 8 0 018-8v8H4z"
+                      ></path>
+                    </svg>
+                  </div>
+                ) : (
+                  <>
+                    {resultVisible && (
+                      <p className="font-bold text-red-500 dark:text-red-400 flex flex-col text-lg text-center gap-3">
+                        Hasil Diagnosis: <br />
+                        <span className="text-gray-800  dark:text-gray-100">
+                          {diagnosis}
+                        </span>
+                      </p>
+                    )}
+                    {resultVisible && (
+                      <div className="flex justify-center gap-4">
+                        <button
+                          type="button"
+                          onClick={handleReset}
+                          className="bg-red-500 text-white px-4 py-2 rounded mt-4"
+                        >
+                          Reset
+                        </button>
+                      </div>
+                    )}
+                    {!resultVisible && (
+                      <div className="py-3">
+                        <p>Apakah Anda yakin dengan jawaban Anda?</p>
+                        <div className="flex justify-between mx-auto items-center">
+                          <button
+                            type="submit"
+                            className="bg-red-500 text-white btn-md hover:bg-red-600 mt-4 rounded-md"
+                          >
+                            Submit
+                          </button>
+                          <button
+                            type="button"
+                            onClick={handleBack}
+                            className="bg-gray-500 dark:bg-slate-600 text-white btn-md mt-4 rounded-md"
+                          >
+                            Kembali
+                          </button>
+                        </div>
+                      </div>
+                    )}
+                  </>
                 )}
               </div>
-              {resultVisible && (
-                <div className="flex justify-center gap-4">
-                  <button
-                    type="button"
-                    onClick={handleReset}
-                    className="bg-red-500 text-white px-4 py-2 rounded mt-4"
-                  >
-                    Reset
-                  </button>
-                </div>
-              )}
-              {!resultVisible && (
-                <div className="py-3">
-                  <p>Apakah Anda yakin dengan jawaban Anda?</p>
-                  <div className="flex justify-between mx-auto items-center">
-                    <button
-                      type="submit"
-                      className="bg-red-500 text-white btn-md hover:bg-red-600 mt-4 rounded-md"
-                    >
-                      Submit
-                    </button>
-                    <button
-                      type="button"
-                      onClick={handleBack}
-                      className="bg-gray-500 dark:bg-slate-600 text-white btn-md mt-4 rounded-md"
-                    >
-                      Kembali
-                    </button>
-                  </div>
-                </div>
-              )}
             </div>
           )}
         </form>
---
+++ src/constants/treatment.jsx
@@ -0,0 +1,32 @@
+export const treatment = [
+  {
+    id: "1",
+    title: "Operasi",
+    description:
+      "Operasi dilakukan dengan membedah tubuh pasien guna mengangkat jaringan kanker. Tindakan ini biasanya dipilih untuk mengatasi kanker stadium awal.",
+  },
+  {
+    id: "2",
+    title: "Kemoterapi",
+    description:
+      "Kemoterapi dilakukan dengan memasukkan obat-obatan ke dalam tubuh pasien melalui intravena atau oral. Perawatan kemoterapi ini bertujuan untuk mengecilkan ukuran serta membunuh sel kanker di dalam tubuh",
+  },
+  {
+    id: "3",
+    title: "Radioterapi",
+    description:
+      "Radioterapi merupakan tindakan medis yang menggunakan sinar X dan proton untuk menghancurkan sel kanker di dalam tubuh. Perawatan radioterapi biasanya dilakukan bersamaan dengan kemoterapi untuk meningkatkan efektivitasnya. Perawatan radioterapi ini juga sering dilakukan setelah tindakan operasi untuk membersihkan sisa-sisa sel kanker.",
+  },
+  {
+    id: "4",
+    title: "Terapi Bertaget",
+    description:
+      "Terapi bertarget (targeted therapy) merupakan metode pengobatan dengan memberikan obat kanker yang berfokus pada kelemahan dari sel kanker secara spesifik tanpa merusak sel-sel sehat di sekitarnya.",
+  },
+  {
+    id: "5",
+    title: "Perawatan Paliatif",
+    description:
+      "Perawatan ini umumnya ditujukan kepada pasien dengan kondisi yang tidak memungkinkan untuk mengikuti pengobatan kanker yang ada. Pada pasien dengan kondisi kanker stadium akhir atau kanker yang sudah menyebar ke jaringan atau organ tubuh lain, perawatan paliatif akan disarankan untuk meringankan penderitaan pasien atau meningkatkan kualitas hidup pasien.",
+  },
+];
